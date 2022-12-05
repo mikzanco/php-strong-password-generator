fbee22a1f814d1de7ba9d789e1cd76f63888e66b
mikzanco <mikyzancolo97@gmail.com>
2022-12-05 09:25:19 +0100

    import function.php dal file function.php al file index.php... MILLESTONE 2

--- index.php
+++ index.php
@@ -6,33 +6,7 @@ if(!isset($_SESSION)){
     session_start();
 }
 
-function generatePsw($caratteri, $lenght) {
-    // $password = ' aaaa ' . $lenght;
-
-    
-
-    $pswrandom = '';
-    if($lenght >=8 && $lenght <= 30){
-        for($i = 0; $i < $lenght; $i++) {
-
-            $random_index = rand(0, 2);
-            $list = $caratteri[$random_index];
-
-            $random_item = rand(0, count($list) - 1);
-            if (is_string($list[$random_item])) {
-                $pswrandom .= strtoupper($list[$random_item]);
-            } else {
-                $pswrandom .= $list[$random_item];
-            }
-        }
-        } else {
-            $pswrandom = 'no';
-        }
-      
-        return $pswrandom;
-      var_dump($pswrandom);
-        
-}
+require_once './function.php';
 
 $caratteri = [
     ['a', 'b', 'c', 'd', 'e ', 'f', 'g', 'h', 'i', 'k', 'j', 'x', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'z', 'w'],
